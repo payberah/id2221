1cb1166ca0d0a77a4cea59c25b012061bec42e25
Amir Payberah <amir@sics.se>
2016-07-12 18:18:18 +0200

    generator

---
+++ lab3/generator/SGen.scala
@@ -0,0 +1,36 @@
+import java.util.Properties
+import kafka.producer.ProducerConfig
+import kafka.javaapi.producer.Producer
+import kafka.producer.KeyedMessage
+import scala.util.Random
+
+object SGen {
+  def main(args: Array[String]): Unit = {
+    val props = new Properties()
+    props.put("metadata.broker.list", "127.0.0.1:9092")
+    props.put("serializer.class", "kafka.serializer.StringEncoder")
+    props.put("request.required.acks", "1")
+
+    val config = new ProducerConfig(props)
+    val producer = new Producer[String, String](config)
+    val topic = "input"
+
+    while (true) {
+      Thread.sleep(10)
+      producer.send(new KeyedMessage[String, String](topic, null, getRandomVal))
+      println("generated: " + getRandomVal)
+    }
+
+    producer.close
+  }
+
+  def getRandomVal: String = {
+    val i = Random.nextInt(alphabet.size)
+    val key = alphabet(i)
+    val value = Random.nextInt(alphabet.size)
+    key + "," + value
+  }
+
+  val alphabet = 'a' to 'z'
+}
+
---
+++ lab3/generator/build.sbt
@@ -0,0 +1,7 @@
+name := "SGen"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.0.0"
